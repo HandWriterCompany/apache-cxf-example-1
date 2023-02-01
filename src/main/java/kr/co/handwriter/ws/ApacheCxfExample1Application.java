3cc44a27e738de0d8059a6e52dd5b4bb66a43d48
Benjamin Kwon <kibong.kwon@handwriter.co.kr>
2023-02-01 13:49:28 +0900

    init

---
+++ src/main/java/kr/co/handwriter/ws/ApacheCxfExample1Application.java
@@ -0,0 +1,13 @@
+package kr.co.handwriter.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCxfExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheCxfExample1Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/handwriter/ws/ApacheCxfExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.handwriter.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCxfExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
